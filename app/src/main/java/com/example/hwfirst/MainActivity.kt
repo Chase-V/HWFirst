b21ada7cea1e517d6684995000046c6ae2d732ca
Andej Nesterenko <mentatus.n@gmail.com>
2021-07-24 19:59:32 +0300

    Продолжаем веселье

--- app/src/main/java/com/example/hwfirst/MainActivity.kt
+++ app/src/main/java/com/example/hwfirst/MainActivity.kt
@@ -2,16 +2,31 @@ package com.example.hwfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+    lateinit var currentButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        currentButton = findViewById(R.id.button1)
+        currentButton.setOnClickListener(this)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            setContentView(R.layout.activity_main_two)
+    }
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.button1 ->{
+                setContentView(R.layout.activity_main_two)
+                currentButton = findViewById(R.id.button2)
+                currentButton.setOnClickListener(this)
+            } R.id.button2 ->{
+                setContentView(R.layout.activity_main)
+                currentButton = findViewById(R.id.button1)
+                currentButton.setOnClickListener(this)
+            }
         }
     }
+
 }
